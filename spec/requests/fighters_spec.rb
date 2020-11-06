89a3504feb56c99cb61d2ade21ac2f4d2fa16b8c
AdamManselll <adammansellhmb@gmail.com>
2020-11-06 08:19:32 -0800

    Added request spec for GET /fighters/:id

--- spec/requests/fighters_spec.rb
+++ spec/requests/fighters_spec.rb
@@ -30,6 +30,36 @@ RSpec.describe "Fighters", type: :request do
     end
   end
 
+  describe "GET /fighters/:id" do
+    let!(:fighter) do
+      Fighter.create(
+        name: 'Adam', 
+        age: 21, 
+        weight: 160, 
+        height: 182,
+        type_of_fighter: "Muay Thai"
+      )
+    end  
+
+    before do
+      get "/fighters/#{fighter.id}"
+    end
+
+    it "returns http success" do      
+      expect(response).to have_http_status(200)
+    end
+
+    it "JSON body response contains expected fighter attributes and their values" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.keys).to match_array(%w[id name age weight height type_of_fighter created_at])
+      expect(json_response['name']).to eq 'Adam'
+      expect(json_response['age']).to eq 21
+      expect(json_response['weight']).to eq 160
+      expect(json_response['height']).to eq 182
+      expect(json_response['type_of_fighter']).to eq "Muay Thai"
+    end      
+  end
+
   describe "POST /fighters" do
     context 'all the required fighter attributes are present in the params' do
       before do
